868f0d75aafa7ed3a87c1de553ace7ede8e74094
421733429@qq.com <421733429@qq.com>
2017-08-13 22:54:55 +0800

    浏览器初步完成

--- WpfOnGithub/MainWindow.xaml.cs
+++ WpfOnGithub/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,35 @@ namespace WpfOnGithub
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //string url = UrlToCommit.Text;
+            string url = "http://www.baidu.com";
+
+            //创建http链接  
+            //HttpWebRequest对象实例:该类用于获取和操作HTTP请求 var可改成HttpWebRequest  
+            var request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url); //Create:创建WebRequest对象  
+
+            //HttpWebResponse对象实例:该类用于获取和操作HTTP应答 var可改成HttpWebResponse  
+            var response = (System.Net.HttpWebResponse)request.GetResponse(); //GetResponse:获取答复  
+
+            //构造数据流对象实例  
+            Stream stream = response.GetResponseStream();//GetResponseStream:获取应答流  
+            StreamReader sr = new StreamReader(stream);  //从字节流中读取字符  
+
+            //从流当前位置读取到末尾并显示在WebBrower控件中  
+            string content = sr.ReadToEnd();
+            Uri uri = new Uri(url);
+            try
+            {
+                WebBrowser1.Navigate(uri);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
